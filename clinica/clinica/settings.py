17dca4dbc158ce0892d06660aaf1608736b6bd0e
Nicroft <ortiznicolas28@gmail.com>
2024-11-01 20:10:54 -0500

    Update settings.py

--- clinica/clinica/settings.py
+++ clinica/clinica/settings.py
@@ -85,23 +85,23 @@ WSGI_APPLICATION = 'clinica.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "mydatabase",
-    }
-}
+#DATABASES = {
+#    "default": {
+#        "ENGINE": "django.db.backends.sqlite3",
+#        "NAME": "mydatabase",
+#    }
+#}
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'TestDB',
-#         'USER': 'postgres',
-#         'PASSWORD': 'laurayjuana12',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'TestDB',
+        'USER': 'postgres',
+         'PASSWORD': 'lulu2804',
+         'HOST': 'localhost',
+        'PORT': '5432',
+     }
+ }
 
 
 # Password validation
